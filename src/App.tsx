c83d0b26bed05639adcc4d646a6f4d3a422d8e7c
Majie <714544413@qq.com>
2022-03-08 11:08:13 +0800

    feat: 添加登录注册

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import "App.css";
-// import { ProjectListScreen } from "screens/project-list";
-// import { TsReactTest } from "try-use-array";
-import { LoginScreen } from "screens/login";
-
+import { useAuth } from "./context/auth-context";
+import { AuthenticatedApp } from "authenticated-app";
+import { UnauthenticatedApp } from "unauthenticated-app";
 function App() {
+  const { user } = useAuth();
   return (
     <div className="App">
-      {/* <ProjectListS
-      
-      creen />
-       */}
-
-      {/* <TsReactTest /> */}
-
-      <LoginScreen />
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
