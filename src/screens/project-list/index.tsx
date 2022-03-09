2f3215c757503a33eb978fafdaae697f464d2970
Majie <714544413@qq.com>
2022-03-09 10:24:26 +0800

    feat: 添加登录维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import List from "./list";
 import SearchPanel from "./search-panel";
-import qs from "qs";
 import { cleanObject, useMount, useDebounce } from "utils";
 import { useState, useEffect } from "react";
-const apiUrl = process.env.REACT_APP_API_URL;
+import { useHttp } from "utils/http";
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: "",
@@ -12,22 +11,13 @@ export const ProjectListScreen = () => {
   });
   const [users, setUsers] = useState([]); //负责人列表
   const [list, setList] = useState([]); //项目列表
+  const client = useHttp();
   const debouncedParam = useDebounce(param, 200);
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
   return (
     <div>
---
+++ src/utils/http.ts
@@ -0,0 +1,52 @@
+import qs from "qs";
+import * as auth from "auth-provider";
+import { useAuth } from "context/auth-context";
+const apiUrl = process.env.REACT_APP_API_URL;
+interface Config extends RequestInit {
+  token?: string;
+  data?: object;
+}
+// 当参数有默认值的时候，将变为可选参数
+export const http = async (
+  endpoint: string,
+  { data, token, headers, ...customConfig }: Config = {}
+) => {
+  const config = {
+    method: "GET",
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": data ? "application/json" : "",
+    },
+    ...customConfig,
+  };
+
+  if (config.method.toUpperCase() === "GET") {
+    endpoint += `?${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
+
+  return window
+    .fetch(`${apiUrl}/${endpoint}`, config)
+    .then(async (response) => {
+      if (response.status === 401) {
+        await auth.logout();
+        window.location.reload();
+        return Promise.reject({ message: "请重新登录" });
+      }
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        //    fetch不能捕获status不为2xx的错误（axios可以）所以需要自己抛出一个错误
+        return Promise.reject(data);
+      }
+    });
+};
+// 上面的方法需要手动传入token，下面的自动传token
+export const useHttp = () => {
+  const { user } = useAuth();
+  // todo讲解ts操作符
+  return (...[endpoint, config]: Parameters<typeof http>) =>
+    http(endpoint, { ...config, token: user?.token });
+};
