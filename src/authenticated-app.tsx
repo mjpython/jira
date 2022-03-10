2272369c802cc93984473d57f4b9844dcb4ea02c
Majie <714544413@qq.com>
2022-03-10 23:36:52 +0800

    style: 优化项目列表页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,24 +2,54 @@ import React from "react";
 import { ProjectListScreen } from "screens/project-list";
 import { useAuth } from "context/auth-context";
 import styled from "@emotion/styled";
+import { Row } from "components/lib";
+import { ReactComponent as SoftwareLogo } from "assets/software-logo.svg";
+import { Dropdown, Menu } from "antd";
 export const AuthenticatedApp = () => {
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
   return (
-    <div>
-      <PageHeader>
-        <button onClick={logout}>登出</button>
-      </PageHeader>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <SoftwareLogo width={"18rem"} color={"rgb(38,132,255)"} />
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <Dropdown
+            overlay={
+              <Menu>
+                <Menu.Item>
+                  <a onClick={logout}>登出</a>
+                </Menu.Item>
+              </Menu>
+            }
+          >
+            <a onClick={(e) => e.preventDefault}>你好,{user?.name}</a>
+          </Dropdown>
+        </HeaderRight>
+      </Header>
       <Main>
         <ProjectListScreen />
       </Main>
-    </div>
+    </Container>
   );
 };
-const PageHeader = styled.header`
-  height: 6rem;
-  background-color: #f00;
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
 `;
 
+const Header = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`;
+
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
 const Main = styled.main`
   height: calc(100vh-6rem);
 `;
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,4 +1,4 @@
-import { Input, Select } from "antd";
+import { Form, Input, Select } from "antd";
 import React from "react";
 export interface User {
   id: string;
@@ -18,34 +18,39 @@ interface SearchPanelProps {
 }
 const SearchPanel = ({ users, setParam, param }: SearchPanelProps) => {
   return (
-    <div>
-      <Input
-        type="text"
-        value={param.name}
-        onChange={(ev) =>
-          setParam({
-            ...param,
-            name: ev.target.value,
-          })
-        }
-      />
-      <Select
-        value={param.personId}
-        onChange={(value) => {
-          setParam({
-            ...param,
-            personId: value,
-          });
-        }}
-      >
-        <Select.Option value="">负责人</Select.Option>
-        {users.map((user) => (
-          <Select.Option key={user.id} value={user.id}>
-            {user.name}
-          </Select.Option>
-        ))}
-      </Select>
-    </div>
+    <Form style={{ marginBottom: "2rem" }} layout={"inline"}>
+      <Form.Item>
+        <Input
+          placeholder="项目名"
+          type="text"
+          value={param.name}
+          onChange={(ev) =>
+            setParam({
+              ...param,
+              name: ev.target.value,
+            })
+          }
+        />
+      </Form.Item>
+      <Form.Item>
+        <Select
+          value={param.personId}
+          onChange={(value) => {
+            setParam({
+              ...param,
+              personId: value,
+            });
+          }}
+        >
+          <Select.Option value="">负责人</Select.Option>
+          {users.map((user) => (
+            <Select.Option key={user.id} value={user.id}>
+              {user.name}
+            </Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+    </Form>
   );
 };
 export default SearchPanel;
