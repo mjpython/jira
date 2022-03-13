b4149e24c358227df3f282d74962d087f68e1857
Majie <714544413@qq.com>
2022-03-13 11:28:51 +0800

    feat: 添加路由projects和project

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,11 +1,12 @@
 import { Table, TableProps } from "antd";
 import dayjs from "dayjs";
 import React from "react";
+import { Link } from "react-router-dom";
 import { User } from "./search-panel";
 export interface Project {
-  id: string;
+  id: number;
   name: string;
-  personId: string;
+  personId: number;
   pin: boolean;
   organization: string;
   created: number;
@@ -20,8 +21,10 @@ const List = ({ users, ...props }: ListProps) => {
       columns={[
         {
           title: "名称",
-          dataIndex: "name",
           sorter: (a, b) => a.name.localeCompare(b.name),
+          render(value, project) {
+            return <Link to={String(project.id)}>{project.name}</Link>;
+          },
         },
         {
           title: "部门",
