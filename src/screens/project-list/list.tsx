ea7db1d778558ba6d312aa124745e5014f33ca51
Majie <714544413@qq.com>
2022-03-15 13:34:32 +0800

    feat: 列表中添加收藏

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,7 +1,9 @@
 import { Table, TableProps } from "antd";
+import { Pin } from "components/pin";
 import dayjs from "dayjs";
 import React from "react";
 import { Link } from "react-router-dom";
+import { useEditProject } from "utils/project";
 import { User } from "./search-panel";
 export interface Project {
   id: number;
@@ -15,10 +17,23 @@ interface ListProps extends TableProps<Project> {
   users: User[];
 }
 const List = ({ users, ...props }: ListProps) => {
+  const { mutate } = useEditProject();
+  const pinProject = (id: number) => (pin: boolean) => mutate({ id, pin });
   return (
     <Table
       pagination={false}
       columns={[
+        {
+          title: <Pin checked={true} disabled={true} />,
+          render(value, project) {
+            return (
+              <Pin
+                checked={project.pin}
+                onCheckedChange={pinProject(project.id)}
+              />
+            );
+          },
+        },
         {
           title: "名称",
           sorter: (a, b) => a.name.localeCompare(b.name),
--- src/utils/project.ts
+++ src/utils/project.ts
@@ -13,3 +13,39 @@ export const useProjects = (param?: Partial<Project>) => {
   }, [param]);
   return result;
 };
+
+// 列表编辑(因为绑定时会传参，但是hook不能在普通函数中（也就是不能在匿名函数中），所以在hook中定义一个方法并返回出去)
+export const useEditProject = () => {
+  const { run, ...asyncResult } = useAsync();
+  const client = useHttp();
+  const mutate = (params: Partial<Project>) => {
+    return run(
+      client(`projects/${params.id}`, {
+        data: params,
+        method: "PATCH",
+      })
+    );
+  };
+  return {
+    mutate,
+    ...asyncResult,
+  };
+};
+
+// 列表添加
+export const useAddProject = () => {
+  const { run, ...asyncResult } = useAsync();
+  const client = useHttp();
+  const mutate = (params: Partial<Project>) => {
+    return run(
+      client(`projects/${params.id}`, {
+        data: params,
+        method: "POST",
+      })
+    );
+  };
+  return {
+    mutate,
+    ...asyncResult,
+  };
+};
